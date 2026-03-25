d5dbc561026412f6503244062fbe150f571ca64a
Lokasenna <solentis1384@gmail.com>
2026-03-25 10:56:48 +0900

    Miscommitted.

--- Traffic Law Enforcement/LocalizationKeys.cs
+++ Traffic Law Enforcement/LocalizationKeys.cs
@@ -16,9 +16,9 @@ namespace Traffic_Law_Enforcement
             map["Tab.Debug"] = setting.GetOptionTabLocaleID(Setting.kDebugTab);
 
             map["Group.General"] = setting.GetOptionGroupLocaleID(Setting.kGeneralGroup);
-            map["Group.BusLaneAuthorized"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLaneAuthorizedGroup);
-            map["Group.BusLaneAdditional"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLaneAdditionalGroup);
-            map["Group.BusLanePressure"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLanePressureGroup);
+            map["Group.PublicTransportLaneAuthorized"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLaneAuthorizedGroup);
+            map["Group.PublicTransportLaneAdditional"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLaneAdditionalGroup);
+            map["Group.PublicTransportLanePressure"] = setting.GetOptionGroupLocaleID(Setting.kPublicTransportLanePressureGroup);
             map["Group.FineAmounts"] = setting.GetOptionGroupLocaleID(Setting.kFineGroup);
             map["Group.RepeatOffenders"] = setting.GetOptionGroupLocaleID(Setting.kRepeatOffenderGroup);
             map["Group.TemplateActions"] = setting.GetOptionGroupLocaleID(Setting.kTemplateActionsGroup);
